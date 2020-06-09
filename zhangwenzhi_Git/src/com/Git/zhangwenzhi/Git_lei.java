f1c5dcc0f35644738a60ca6fe0b498fc2c0b1545
zhangwenzhi2020 <237453498@qq.com>
2020-06-09 17:25:25 +0800

    提交到本地仓库

---
+++ zhangwenzhi_Git/src/com/Git/zhangwenzhi/Git_lei.java
@@ -0,0 +1,19 @@
+package com.Git.zhangwenzhi;
+
+public class Git_lei {
+
+	public	int id;
+	public  String name;
+	private float shu;
+	
+	public void biubiubiu() {
+		 name = "zhangwenzhi";
+		
+		System.out.print(name);
+	
+	}
+	
+	
+	
+	
+}
